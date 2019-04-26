8d5275905b8f3a4c24e56b4bc80d74c4aed26ddf
xoan1995 <hoanglapnhue@gmail.com>
2019-04-26 17:46:03 +0800

    doi tien

---
+++ exchange.js
@@ -0,0 +1,9 @@
+function Sotien() {
+let input= parseFloat(document.getElementById("output").value);
+let select1= document.getElementById("from").value;
+let select2= document.getElementById("to").value;
+let KQ=input*select2/select1;
+alert(KQ);
+
+
+}
